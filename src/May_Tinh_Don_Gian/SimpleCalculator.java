5d9e93197cebe61ad151b1c3365f95997f03d23f
nguyenvanbinh981988 <binhnguyenvan981988@gmail.com>
2022-05-19 10:08:40 +0700

    new

---
+++ src/May_Tinh_Don_Gian/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package May_Tinh_Don_Gian;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
