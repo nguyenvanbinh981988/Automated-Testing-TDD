c2e0e843f9f591852fa5a042d2ce619e141faed2
nguyenvanbinh981988 <binhnguyenvan981988@gmail.com>
2022-05-19 16:29:02 +0700

    new

--- src/TriangleClassifier/TriangleClassifierClass.java
+++ src/TriangleClassifier/TriangleClassifierClass.java
@@ -2,7 +2,7 @@ package TriangleClassifier;
 
 public class TriangleClassifierClass {
     public String creatTriangle(double a, double b, double c){
-        boolean Triangle = a+b>=c && a+c>b && b+c > a;
+        boolean Triangle = a+b>=c && a+c>b && b+c > a && a > 0 && b >0 && c > 0;
         if (Triangle){
             if (a == b && b == c){
                 return "this is a equilateral triangle";
